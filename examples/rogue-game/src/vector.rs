42f42ab433c31e4a8e218746ae713ab7375d211e
jprugel <jonathon.prugel@gmail.com>
2026-02-07 07:14:28 -0500

    Finished Vector Math

--- examples/rogue-game/src/vector.rs
+++ examples/rogue-game/src/vector.rs
@@ -26,3 +26,23 @@ where
         (pow(self.x, 2) + pow(self.y, 2)).sqrt()
     }
 }
+
+trait Distance {
+    type Output;
+
+    fn distance(self, other: Self) -> Self::Output;
+}
+
+impl<T> Distance for Vector2<T>
+where
+    T: Num + Clone,
+    T: Root<Output = T>,
+    T: Mul<Output = T>,
+    T: One<Output = T>,
+{
+    type Output = T;
+
+    fn distance(self, other: Self) -> Self::Output {
+        (pow(self.x - other.x, 2) + pow(self.y - other.y, 2)).sqrt()
+    }
+}
